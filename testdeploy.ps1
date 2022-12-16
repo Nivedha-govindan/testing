70c8224178f5dcd20313d5cee8a736919cd9663d
Nivedha G <109720688+Nivedha-govindan@users.noreply.github.com>
2022-12-16 12:44:06 +0530

    Update testdeploy.ps1

--- testdeploy.ps1
+++ testdeploy.ps1
@@ -10,5 +10,4 @@ if ((Get-Module -ListAvailable Az.Accounts) -eq $null)
     Install-Module -Name Az.Accounts -Force
 }
 
-Write-Output "Task: Generating Databricks Token"
-$WORKSPACE_ID = (az resource show --resource-type Microsoft.Databricks/workspaces --resource-group $RG_NAME --name $WORKSPACE_NAME --query id --output tsv)
+Write-Output "AZ is installed"
