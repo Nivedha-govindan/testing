beb0615ee11af22d9694fcca9d2836189d2a9ec1
Nivedha G <109720688+Nivedha-govindan@users.noreply.github.com>
2022-12-19 11:10:14 +0530

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -4,10 +4,12 @@ param(
 [string] $WORKSPACE_NAME
 )
 
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
 if ((Get-Module -ListAvailable Az.Accounts) -eq $null)
-{
-    Install-Module -Name Az.Accounts -Force
-}
+	{
+       Install-Module -Name Az.Accounts -Force
+    }
 
 Write-Output "Task: Generating Databricks Token"
 $WORKSPACE_ID = (az resource show --resource-type Microsoft.Databricks/workspaces --resource-group $RG_NAME --name $WORKSPACE_NAME --query id --output tsv)
