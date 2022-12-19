f890121389e60f414ef60965a3d6bd62295660b8
Nivedha G <109720688+Nivedha-govindan@users.noreply.github.com>
2022-12-19 15:23:18 +0530

    Update deploy1.ps1

--- deploy1.ps1
+++ deploy1.ps1
@@ -3,10 +3,6 @@ param(
 [string] $WORKSPACE_NAME
 )
 
-if ((Get-Module -ListAvailable Az.Accounts) -eq $null)
-{
-    Install-Module -Name Az.Accounts -Force
-}
 
 Write-Output "Task: Generating Databricks Token"
 $WORKSPACE_ID = (az resource show --resource-type Microsoft.Databricks/workspaces --resource-group $RG_NAME --name $WORKSPACE_NAME --query id --output tsv)
